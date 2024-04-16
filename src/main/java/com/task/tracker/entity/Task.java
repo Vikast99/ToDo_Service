3cd3857d8eb08788c7b013f26faaae6aa89db41d
Vikast@99 <vikash.tiwari@intelliatech.com>
2024-04-16 13:28:17 +0530

    next commit

--- src/main/java/com/task/tracker/entity/Task.java
+++ src/main/java/com/task/tracker/entity/Task.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "task")
 @Getter
 @Setter
 @AllArgsConstructor
