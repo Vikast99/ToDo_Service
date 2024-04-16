4d9340b9f587d5b4b6f08a8a445b45ce92f7f3c1
harshit123vyas <121675255+harshit-vyas@users.noreply.github.com>
2024-04-16 11:06:29 +0530

    first commit

--- src/main/java/com/task/tracker/serviceImp/TaskServiceImp.java
+++ src/main/java/com/task/tracker/serviceImp/TaskServiceImp.java
@@ -9,6 +9,9 @@ import com.task.tracker.entity.Task;
 import com.task.tracker.repository.TaskRepository;
 import com.task.tracker.service.TaskService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TaskServiceImp implements TaskService{
 	
 	@Autowired
@@ -16,13 +19,18 @@ public class TaskServiceImp implements TaskService{
 
 	@Override
 	public List<Task> getAllTask() {
-		List<Task> taskList=taskRepository.findAll();
-		if(!taskList.isEmpty()) {
-			return taskList;
+		
+		try {
+			List<Task> taskList=taskRepository.findAll();
+			if(!taskList.isEmpty()) {
+				return taskList;
+			}
+			
+		}catch (Exception e) {
+			log.error("exception "+ e);
 		}
-		else {
+		
 			return null;
-		}
 	
 	}
 
@@ -37,15 +45,23 @@ public class TaskServiceImp implements TaskService{
 
 	@Override
 	public Task addTask(Task task) {
-		return taskRepository.save(task);
-		
-		
+		return taskRepository.save(task);	
 		}
 		
 	
 	@Override
-	public Task updateTask(Task task, Integer id) {
+	public Task updateTask(Task newTask, Integer id) {
 		
+		Optional<Task> task = taskRepository.findById(id);
+		if(task.isPresent()) {
+			if(!newTask.getTitle().equals(null)) {
+				task.get().setTitle(newTask.getTitle());
+			}
+			if(!newTask.getDescription().equals(null)) {
+				task.get().setDescription(newTask.getDescription());
+			}
+			taskRepository.save(task.get());
+		}
 		return null;
 	}
 
